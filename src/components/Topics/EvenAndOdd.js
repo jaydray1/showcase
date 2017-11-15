8b1eff028660622cf73ca0547e2b47cc83e61a55
JM <jdreher15@gmail.com>
2017-11-15 14:28:29 -0700

    solved evens and odds using filter function

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -13,6 +13,19 @@ export default class EvenAndOdd extends Component {
     handleChange(val){
         this.setState({userInput: val})
     }
+    solveArrays() {
+        var stringToArray = this.state.userInput.split(",").map(Number);
+        var odds = [];
+        const evens = stringToArray.filter((e) => {
+            if(e % 2 === 0) {
+                return true;
+            } else {
+                odds.push(e)
+            }
+            return evens;
+        })
+        this.setState({evenArray: evens, oddArray: odds})
+    }
     render() {
         return (
             <div className="puzzleBox evenAndOddPB">
@@ -22,9 +35,12 @@ export default class EvenAndOdd extends Component {
                 className="inputLine"
                 onChange={(e) => this.handleChange(e.target.value)}
                 />
-                <button className="confirmationButton"></button>
-                <span className="resultsBox"></span>
-                <span className="resultsBox"></span>
+                <button 
+                className="confirmationButton"
+                onClick={() => this.solveArrays()}
+                >Split</button>
+                <span className="resultsBox"> Evens: {JSON.stringify(this.state.evenArray)} </span>
+                <span className="resultsBox"> Odds: {JSON.stringify(this.state.oddArray)} </span>
             </div>
         )
     }
