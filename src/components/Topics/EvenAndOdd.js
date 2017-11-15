774ea92ccde7d60d19e178aea3382514153bf5c5
JM <jdreher15@gmail.com>
2017-11-15 11:50:17 -0700

    business logic working on

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
 export default class EvenAndOdd extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            evenArray: [],
+            oddArray: [],
+            userInput: ''
+        }
+    }
+    handleChange(val){
+        this.setState({userInput: val})
+    }
     render() {
         return (
-            <p>Even and Odds</p>
+            <div className="puzzleBox evenAndOddPB">
+                <h4>Evens and Odds</h4>
+                <input 
+                type="text" 
+                className="inputLine"
+                onChange={(e) => this.handleChange(e.target.value)}
+                />
+                <button className="confirmationButton"></button>
+                <span className="resultsBox"></span>
+                <span className="resultsBox"></span>
+            </div>
         )
     }
 }
